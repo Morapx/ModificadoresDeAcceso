910a1aa5b24a55b40937cc780dd5a45643796251
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-12 12:44:50 -0700

    Agregar archivos de proyecto.

---
+++ ModificadoresDeAcceso/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public DateTime FechaDeNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+
+        private string _curp;
+        public string Curp
+        {
+            get
+            {
+                return _curp;
+            }
+            set
+            {
+                if (value.Length == 18)
+                {
+                    _curp = value;
+                }
+                else 
+                {
+                    throw new Exception("No es de 18 caracteres");
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ ModificadoresDeAcceso/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadoresDeAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente
+                = new Cliente();
+
+            cliente.Nombre = "Emiliano zapata";
+            cliente.Telefono = "644521584";
+
+            Console.WriteLine("Nombre: " + cliente.Nombre);
+            Console.WriteLine("Telefono " + cliente.Telefono);
+            try
+            {
+
+                cliente.Curp ="111111111111111111";
+                Console.WriteLine("Curp: " + cliente.Curp);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("La Curp debe ser de 18 caracteres");
+
+            }
+
+
+            Console.Read();
+        }
+    }
+}
